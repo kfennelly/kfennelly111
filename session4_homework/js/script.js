13d8d1a88dfbd16cb188f5ba4ca2c0fd1d1ec2a9
kfennelly <kfennelly1@gmail.com>
2017-01-21 11:39:34 -0600

    Update script.js

--- session4_homework/js/script.js
+++ session4_homework/js/script.js
@@ -1,13 +1,10 @@
 // fade in h1 and photos
 $(document).ready(function(){
   $("h1, header").fadeIn(5000);
+  })
   $("header").click(function(){
     $("header").css("background-color", "lightgray");
-  });
-});
-// click to hide cactus photo
-$(document).ready(function(){
-    $("#section1img").click(function(){
-        $(this).fadeToggle(1000);
-    });
+  })
+  $("#section1img").click(function(){
+    $(this).fadeToggle(1000);
 });
