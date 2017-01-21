77e165c41ff7f78b30affc0cb96f534394b5a8e0
kfennelly <kfennelly1@gmail.com>
2017-01-21 11:45:39 -0600

    Update script.js

--- session4_homework/js/script.js
+++ session4_homework/js/script.js
@@ -1,6 +1,6 @@
 // fade in h1 and photos, click to change header color
 $(document).ready(function(){
-  $("h1, img, header").fadeIn(5000);
-  $("header").click(function(){
-    $("header").css("background-color", "lightgray");
+  $("h1, header").fadeIn(5000);
+  /* $("header").click(function(){
+    $("header").css("background-color", "lightgray"); */
 });
