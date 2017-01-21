5cbad6621d7c9860e5ebd0241765ba3a76decd19
kfennelly <kfennelly1@gmail.com>
2017-01-21 11:42:32 -0600

    Update script.js

--- session4_homework/js/script.js
+++ session4_homework/js/script.js
@@ -1,6 +1,6 @@
 // fade in h1 and photos
 $(document).ready(function(){
-  $("h1, section1img, header").fadeIn(5000);
+  $("h1, img, header").fadeIn(5000);
   });
   $("header").click(function(){
     $("header").css("background-color", "lightgray");
